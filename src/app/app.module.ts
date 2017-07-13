64a1e991ebb0cdd6268e1bfc287caebac0a084e2
Никита Кочетков <n.kochetkov@isimplelab.com>
2017-07-13 18:01:43 +0300

    Add match details info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,42 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpModule } from '@angular/http';
+import { CloudSettings, CloudModule } from '@ionic/cloud-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ApiService } from "../services/api.service";
+import { MatchDetails } from "../pages/match/match.details";
+
+const cloudSettings: CloudSettings = {
+  'core': {
+    'app_id': '7149379c'
+  }
+};
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    MatchDetails
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    CloudModule.forRoot(cloudSettings),
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    MatchDetails
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    ApiService,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ModalController, NavParams, Platform, LoadingController } from 'ionic-angular';
+import { ApiService } from "../../services/api.service";
+import { MatchDetails } from "../match/match.details";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,86 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  version: string;
+  nickname: string;
+  region: string;
+  matches: any;
+
+  constructor(public navCtrl: NavController,
+              private api: ApiService,
+              public modalCtrl: ModalController,
+              public loadingCtrl: LoadingController) {
+    this.version = '0.0.1';
+  }
+
+  searchSummoner() {
+    let loading = this.loadingCtrl.create({
+      content: 'Загрузка данных...'
+    });
+
+    loading.present();
+    console.log(this.nickname, this.region);
+    this.api.getUserId(this.nickname).subscribe(res => {
+      console.log(res);
+      this.api.getMatchList().subscribe(res => {
+        console.log(res);
+        this.matches = res.games;
+        let self = this;
+        this.matches.forEach(function (item) {
+          item.itemImages = [
+            {
+              link: `http://ddragon.leagueoflegends.com/cdn/6.24.1/img/item/${item.stats.item0}.png ` || '',
+              key: item.stats.item0 || null
+            },
+            {
+              link: `http://ddragon.leagueoflegends.com/cdn/6.24.1/img/item/${item.stats.item1}.png ` || '',
+              key: item.stats.item1 || null
+            },
+            {
+              link: `http://ddragon.leagueoflegends.com/cdn/6.24.1/img/item/${item.stats.item2}.png ` || '',
+              key: item.stats.item2 || null
+            },
+            {
+              link: `http://ddragon.leagueoflegends.com/cdn/6.24.1/img/item/${item.stats.item3}.png ` || '',
+              key: item.stats.item3 || null
+            },
+            {
+              link: `http://ddragon.leagueoflegends.com/cdn/6.24.1/img/item/${item.stats.item4}.png ` || '',
+              key: item.stats.item4 || null
+            },
+            {
+              link: `http://ddragon.leagueoflegends.com/cdn/6.24.1/img/item/${item.stats.item5}.png ` || '',
+              key: item.stats.item5 || null
+            },
+            {
+              link: `http://ddragon.leagueoflegends.com/cdn/6.24.1/img/item/${item.stats.item6}.png ` || '',
+              key: item.stats.item6 || null
+            },
+          ];
+          self.api.getChampById(item.championId).subscribe(res => {
+            item.champName = res.name;
+            item.champTitle = res.title;
+            item.champImg = `http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/${res.key}.png`;
+            item.champKey = res.key;
+          });
+        });
+        console.log(this.matches);
+        loading.dismiss();
+      })
+    });
+  }
+
+  itemDetails(key: any) {
 
   }
 
+  matchDetails(id: any) {
+    this.navCtrl.push(MatchDetails, {
+      details: id
+    });
+  }
+
 }
+
+
+
